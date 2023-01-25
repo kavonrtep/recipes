996772e280e96efabe9f6f1e784bf453be46c245
kavonrtep <petr@umbr.cas.cz>
2023-01-25 09:46:34 +0100

    dante_lt recipe version update 0.2.2.0

--- dante/build.sh
+++ dante/build.sh
@@ -13,5 +13,11 @@ cp -r * ${DANTE_DIR}
 # link executables
 ln -s ${DANTE_DIR}/dante ${PREFIX}/bin/dante
 ln -s ${DANTE_DIR}/dante_gff_output_filtering.py ${PREFIX}/bin/dante_gff_output_filtering.py
+ln -s ${DANTE_DIR}/coverage2gff.py ${PREFIX}/bin/coverage2gff.py
+ln -s ${DANTE_DIR}/dante_gff_to_dna.py ${PREFIX}/bin/dante_gff_to_dna.py
+ln -s ${DANTE_DIR}/fasta2database.py ${PREFIX}/bin/fasta2database.py
+ln -s ${DANTE_DIR}/parse_aln.py ${PREFIX}/bin/parse_aln.py
+ln -s ${DANTE_DIR}/summarize_gff.R ${PREFIX}/bin/summarize_gff.R
+
 
 
