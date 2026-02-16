c45b805b9db9054202f9079eeab8dc339ebb8b42
Petr Novak <petr@umbr.cas.cz>
2026-02-16 15:00:54 +0100

    dante_tir updated

--- tidecluster/build.sh
+++ tidecluster/build.sh
@@ -14,4 +14,10 @@ cp -r * ${TIDECLUSTER}
 ln -s ${TIDECLUSTER}/TideCluster.py ${PREFIX}/bin/TideCluster.py
 ln -s ${TIDECLUSTER}/tc_update_gff3.py ${PREFIX}/bin/tc_update_gff3.py
 ln -s ${TIDECLUSTER}/tc_reannotate.py ${PREFIX}/bin/tc_reannotate.py
+ln -s ${TIDECLUSTER}/tc_merge_annotations.py ${PREFIX}/bin/tc_merge_annotations.py
+ln -s ${TIDECLUSTER}/tc_utils.py ${PREFIX}/bin/tc_utils.py
+ln -s ${TIDECLUSTER}/tc_comparative_analysis.R ${PREFIX}/bin/tc_comparative_analysis.R
+ln -s ${TIDECLUSTER}/tc_summarize_comparative_analysis.R ${PREFIX}/bin/tc_summarize_comparative_analysis.R
+
+
 
